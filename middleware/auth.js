68e1afe5ab56f8bc77aa5d25c1585342425f1db7
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-10-14 11:01:54 +0530

    DB connection

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        // Verify the admin token
+        const adminPayload = jwt.verify(token, process.env.ADMIN_KEY);
+        // Proceed with the request for admin
+        req.user = adminPayload;
+        next();
+    } catch (adminError) {
+        try {
+            // Verify the user token
+            const userPayload = jwt.verify(token, process.env.USER_KEY);
+
+            // Proceed with the request for user
+            req.user = userPayload;
+            next();
+        } catch (userError) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+    }
+};
+module.exports = authenticateToken;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs')
+var User = require('../Model/User')
+const JWT = require('jsonwebtoken')
+const authenticateToken = require('../middleware/auth');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, type } = req.body;
+    console.log(req.body)
+    // Check if user already exists
+    let existing = await User.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new teacher
+    const newUser = new User({ name, email, password: hashedPassword, type });
+    console.log(newUser)
+    // Save teacher to database
+    const user = await newUser.save();
+
+    // Create and sign JWT token
+    JWT.sign({ id: user._id }, process.env.USER_KEY, (err, token) => {
+      if (err) {
+        res.json({ error: "somthing went wrong" })
+      }
+      res.json({ auth: token })
+    })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send(error);
+  }
+})
+
+router.get('/get', async(req, res) => {
+  try {
+    const userinfo = await User.find();
+    res.json(userinfo)
+  } catch (error) {
+    res.json(error)
+  }
+})
 module.exports = router;
