1ef6e56ce5a72682d032ee0eaca351dd58e00503
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-10-15 16:36:41 +0530

    updated home

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+const User = require('../Model/User');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/home', function(req, res, next) {
-  res.send({ title: 'Express' });
+router.get('/home', async(req, res, next)=> {
+  let data = await User.find()
+  res.send(data)
 });
 
 module.exports = router;
